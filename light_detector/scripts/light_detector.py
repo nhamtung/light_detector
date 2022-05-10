e5f5e0233ec33344bfab048e46a196bbbb51ae0a
robotics <nhamtung>
2022-05-10 22:35:32 +0700

    upsate

--- light_detector/scripts/light_detector.py
+++ light_detector/scripts/light_detector.py
@@ -5,10 +5,10 @@ import time
 
 # cap = cv2.VideoCapture(0)
 # cap = cv2.VideoCapture('/home/robotics/Downloads/MusicLEDBox.mp4')
-frame = cv2.imread('/home/robotics/Downloads/image/13.jpg')
 
 while True:
     # _, frame = cap.read()
+    frame = cv2.imread('/home/robotics/Downloads/image/17.jpg')
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (9,9), 0, sigmaY=0, borderType=cv2.BORDER_DEFAULT)
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(blur)
@@ -18,7 +18,7 @@ while True:
 
     edged = cv2.Canny(threshold, 50, 150)
     lightcontours = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1.0, 100, param1=100, param2=30, minRadius=5, maxRadius=50)
+    circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1.0, 30, param1=60, param2=30, minRadius=13, maxRadius=50)
     if len(lightcontours)>0 and circles is not None:
         count = 0
         circles = np.uint16(np.around(circles))
@@ -28,7 +28,7 @@ while True:
             # print("(a, b) = (" + str(a) + ", " + str(b) +")")
             count = count +1
             # frame = cv2.putText(frame, str(count), (a-15, b+5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
-        print("number of circles: " + str(count))
+        # print("number of circles: " + str(count))
 
         # maxcontour = max(lightcontours, key=cv2.contourArea)
         # if cv2.contourArea(maxcontour) > 2000:
@@ -36,8 +36,8 @@ while True:
         #     cv2.circle(frame, (int(x), int(final_y)), int(radius), (0, 255, 0), 4)
         #     cv2.rectangle(frame, (int(x) - 5, int(final_y) - 5), (int(x) + 5, int(final_y) + 5), (0, 128, 255), -1)
 
-    cv2.imshow('light', thr)
     cv2.imshow('frame', frame)
+    cv2.imshow('light', thr)
     # cv2.imshow('gray', gray)
     # cv2.imshow('blur', blur)
     cv2.waitKey(4)
