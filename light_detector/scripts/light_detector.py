fd37dac3bb2d53049bc9f878a2ae3d3cb29d95df
robotics <nhamtung>
2022-05-11 23:46:47 +0700

    save

--- light_detector/scripts/light_detector.py
+++ light_detector/scripts/light_detector.py
@@ -16,30 +16,34 @@ while True:
     thr = threshold.copy()
     cv2.resize(thr, (200,200))
 
-    edged = cv2.Canny(threshold, 50, 150)
-    lightcontours = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    gray_edged = cv2.Canny(gray, 50, 150)
     circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1.0, 30, param1=60, param2=30, minRadius=13, maxRadius=50)
-    if len(lightcontours)>0 and circles is not None:
-        count = 0
+    if circles is not None:
         circles = np.uint16(np.around(circles))
         for pt in circles[0, :]:
             a, b, r = pt[0], pt[1], pt[2]
             cv2.circle(frame, (a, b), 40, (0, 255, 0), 2)
             # print("(a, b) = (" + str(a) + ", " + str(b) +")")
-            count = count +1
-            # frame = cv2.putText(frame, str(count), (a-15, b+5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
-        # print("number of circles: " + str(count))
+            # frame = cv2.putText(frame, "1", (a-15, b+5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
 
-        # maxcontour = max(lightcontours, key=cv2.contourArea)
-        # if cv2.contourArea(maxcontour) > 2000:
-        #     (x, final_y), radius = cv2.minEnclosingCircle(maxcontour)
-        #     cv2.circle(frame, (int(x), int(final_y)), int(radius), (0, 255, 0), 4)
-        #     cv2.rectangle(frame, (int(x) - 5, int(final_y) - 5), (int(x) + 5, int(final_y) + 5), (0, 128, 255), -1)
+    edged = cv2.Canny(threshold, 100, 200)
+    lightcontours = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # print(len(lightcontours))
+    # if len(lightcontours)!=0:
+    #     maxcontour = max(lightcontours, key=cv2.contourArea)
+    #     if cv2.contourArea(maxcontour) > 2000:
+    #         (x, final_y), radius = cv2.minEnclosingCircle(maxcontour)
+    #         cv2.circle(frame, (int(x), int(final_y)), int(radius), (0, 255, 0), 4)
+    #         cv2.rectangle(frame, (int(x) - 5, int(final_y) - 5), (int(x) + 5, int(final_y) + 5), (0, 128, 255), -1)
 
+    # cv2.imshow('gray', gray)
+    # cv2.imshow('gray_edged', gray_edged)
+    # cv2.imshow('blur', blur)
     cv2.imshow('frame', frame)
     cv2.imshow('light', thr)
-    # cv2.imshow('gray', gray)
-    # cv2.imshow('blur', blur)
+    # cv2.imshow('edged', edged)
+    # cv2.imshow('lightCircles', lightCircles)
+    
     cv2.waitKey(4)
     key = cv2.waitKey(5) & 0xFF
     if key == ord('q'):
